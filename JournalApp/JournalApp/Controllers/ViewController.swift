cdc2aed12bf78ba2e79a8bdd789e52a3a2ff3169
Kevin Watke <kjwatke@gmail.com>
2022-02-04 09:05:55 -0600

    setup data for tableView

--- JournalApp/JournalApp/Controllers/ViewController.swift
+++ JournalApp/JournalApp/Controllers/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var tableView: UITableView!
+	
+	private var notes = [Note]()
 	override func viewDidLoad() {
+		
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		tableView.delegate = self
+		tableView.dataSource = self
+		
 	}
 
 
 }
+
+// MARK: - TableView DataSource Methods
+
+extension ViewController: UITableViewDataSource {
+	
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		
+		return notes.count
+		
+	}
+	
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
+		let cell = tableView.dequeueReusableCell(withIdentifier: "NoteCell", for: indexPath)
+		
+		return cell
+		
+	}
+	
+	
+}
+
+
+// MARK: - TableView Delegate Methods
+
+extension ViewController: UITableViewDelegate {
+	
+	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		
+	}
+	
+}
+
 
